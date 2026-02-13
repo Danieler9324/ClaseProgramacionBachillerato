5813cc367aa51698fc173bfa3e5cc502907b7ad4
danieler9324 <danielviramontes562@gmail.com>
2026-02-12 18:06:51 -0600

    continuidad en las operaciones Version 3

--- sexto semestre C#/Calculadora/Calculadora/Program.cs
+++ sexto semestre C#/Calculadora/Calculadora/Program.cs
@@ -4,21 +4,20 @@ class Program
     static void Main(string[] args)
     {
         bool seguir;
-
+        double num;
+        double resultado = 0;
+        char operador;
+        string opInput;
+        // Solicitar al usuario que ingrese dos números
+        Console.Write("Ingrese el primer numero: ");
+        while (!double.TryParse(Console.ReadLine(), out resultado))
+        {
+            Console.Write("Entrada inválida. Ingrese el primer numero nuevamente: ");
+        }
         do
         {
-
-            double num1, num2, resultado;
-            char operador;
-            string opInput;
-            // Solicitar al usuario que ingrese dos números
-            Console.Write("Ingrese el primer numero: ");
-            while (!double.TryParse(Console.ReadLine(), out num1))
-            {
-                Console.Write("Entrada inválida. Ingrese el primer numero nuevamente: ");
-            }
             Console.Write("Ingrese el segundo numero: ");
-            while (!double.TryParse(Console.ReadLine(), out num2))
+            while (!double.TryParse(Console.ReadLine(), out num))
             {
                 Console.Write("Entrada inválida. Ingrese el segundo numero nuevamente: ");
             }
@@ -42,37 +41,35 @@ class Program
             switch (operador)
             {
                 case '+':
-                    resultado = num1 + num2;
+                    resultado += num;
                     break;
                 case '-':
-                    resultado = num1 - num2;
+                    resultado -= num;
                     break;
                 case '*':
-                    resultado = num1 * num2;
+                    resultado *= num;
                     break;
                 case '/':
                     // Verificar si el divisor es 0
-                    while (num2 == 0)
+                    while (num == 0)
                     {
                         Console.WriteLine("Para division el segundo numero no puede ser 0");
 
                         Console.Write("Ingrese el segundo numero nuevamente: ");
-                        while (!double.TryParse(Console.ReadLine(), out num2))
+                        while (!double.TryParse(Console.ReadLine(), out num))
                         {
                             Console.Write("Entrada invalida. Intente nuevamente: ");
                         }
                     }
 
-                    resultado = num1 / num2;
+                    resultado /= num;
                     break;
                 default:
                     Console.Error.WriteLine("Error: Operador no válido.");
                     return;
             }
 
-            // Mostrar el resultado
             Console.WriteLine($"Resultado: {resultado}");
-
 
             Console.WriteLine("Quieres continuar? ( S / N ) ");
             char opcion = Char.ToUpper(Console.ReadKey().KeyChar);
