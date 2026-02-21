b2e2c96d4eb82473fd587ff1d7e9cdab05121051
danieler9324 <danielviramontes562@gmail.com>
2026-02-20 21:57:25 -0600

    Se agrego: Gato version 1

---
+++ sexto semestre C#/GatoVesao2/GatoVesao2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GatoVesao2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            b00 = new Button();
+            b01 = new Button();
+            b02 = new Button();
+            b10 = new Button();
+            b11 = new Button();
+            b12 = new Button();
+            b20 = new Button();
+            b21 = new Button();
+            b22 = new Button();
+            SuspendLayout();
+            // 
+            // b00
+            // 
+            b00.Font = new Font("Segoe UI", 20F);
+            b00.Location = new Point(354, 111);
+            b00.Name = "b00";
+            b00.Size = new Size(100, 100);
+            b00.TabIndex = 0;
+            b00.UseVisualStyleBackColor = true;
+            b00.Click += b00_Click;
+            // 
+            // b01
+            // 
+            b01.Font = new Font("Segoe UI", 20F);
+            b01.Location = new Point(473, 111);
+            b01.Name = "b01";
+            b01.Size = new Size(100, 100);
+            b01.TabIndex = 1;
+            b01.UseVisualStyleBackColor = true;
+            b01.Click += b01_Click;
+            // 
+            // b02
+            // 
+            b02.Font = new Font("Segoe UI", 20F);
+            b02.Location = new Point(592, 111);
+            b02.Name = "b02";
+            b02.Size = new Size(100, 100);
+            b02.TabIndex = 2;
+            b02.UseVisualStyleBackColor = true;
+            b02.Click += b02_Click;
+            // 
+            // b10
+            // 
+            b10.Font = new Font("Segoe UI", 20F);
+            b10.Location = new Point(354, 234);
+            b10.Name = "b10";
+            b10.Size = new Size(100, 100);
+            b10.TabIndex = 3;
+            b10.UseVisualStyleBackColor = true;
+            b10.Click += b10_Click;
+            // 
+            // b11
+            // 
+            b11.Font = new Font("Segoe UI", 20F);
+            b11.Location = new Point(473, 234);
+            b11.Name = "b11";
+            b11.Size = new Size(100, 100);
+            b11.TabIndex = 4;
+            b11.UseVisualStyleBackColor = true;
+            b11.Click += b11_Click;
+            // 
+            // b12
+            // 
+            b12.Font = new Font("Segoe UI", 20F);
+            b12.Location = new Point(592, 234);
+            b12.Name = "b12";
+            b12.Size = new Size(100, 100);
+            b12.TabIndex = 5;
+            b12.UseVisualStyleBackColor = true;
+            b12.Click += b12_Click;
+            // 
+            // b20
+            // 
+            b20.Font = new Font("Segoe UI", 20F);
+            b20.Location = new Point(354, 352);
+            b20.Name = "b20";
+            b20.Size = new Size(100, 100);
+            b20.TabIndex = 6;
+            b20.UseVisualStyleBackColor = true;
+            b20.Click += b20_Click;
+            // 
+            // b21
+            // 
+            b21.Font = new Font("Segoe UI", 20F);
+            b21.Location = new Point(473, 352);
+            b21.Name = "b21";
+            b21.Size = new Size(100, 100);
+            b21.TabIndex = 7;
+            b21.UseVisualStyleBackColor = true;
+            b21.Click += b21_Click;
+            // 
+            // b22
+            // 
+            b22.Font = new Font("Segoe UI", 20F);
+            b22.Location = new Point(592, 352);
+            b22.Name = "b22";
+            b22.Size = new Size(100, 100);
+            b22.TabIndex = 8;
+            b22.UseVisualStyleBackColor = true;
+            b22.Click += b22_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1088, 554);
+            Controls.Add(b22);
+            Controls.Add(b21);
+            Controls.Add(b20);
+            Controls.Add(b12);
+            Controls.Add(b11);
+            Controls.Add(b10);
+            Controls.Add(b02);
+            Controls.Add(b01);
+            Controls.Add(b00);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button b00;
+        private Button b01;
+        private Button b02;
+        private Button b10;
+        private Button b11;
+        private Button b12;
+        private Button b20;
+        private Button b21;
+        private Button b22;
+    }
+}
---
+++ sexto semestre C#/GatoVesao2/GatoVesao2/Form1.cs
@@ -0,0 +1,134 @@
+namespace GatoVesao2
+{
+    public partial class Form1 : Form
+    {
+        string siguiente = "X";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b00_Click(object sender, EventArgs e)
+        {
+            b00.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+        private void b01_Click(object sender, EventArgs e)
+        {
+            b01.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+        private void b02_Click(object sender, EventArgs e)
+        {
+            b02.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+        private void b10_Click(object sender, EventArgs e)
+        {
+            b10.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+        private void b11_Click(object sender, EventArgs e)
+        {
+            b11.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+        private void b12_Click(object sender, EventArgs e)
+        {
+            b12.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+        private void b20_Click(object sender, EventArgs e)
+        {
+            b20.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+        private void b21_Click(object sender, EventArgs e)
+        {
+            b21.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+        private void b22_Click(object sender, EventArgs e)
+        {
+            b22.Text = siguiente;
+
+            if (siguiente == "X")
+            {
+                siguiente = "O";
+            }
+            else
+            {
+                siguiente = "X";
+            }
+        }
+    }
+}
