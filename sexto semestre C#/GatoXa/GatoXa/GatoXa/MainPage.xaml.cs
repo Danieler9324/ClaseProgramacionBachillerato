84595e1cee479e47c4ff01ec9cbce948467f401b
danieler9324 <danielviramontes562@gmail.com>
2026-04-21 14:29:55 -0600

    Tres en raya para android (Xamarin) agregado

---
+++ sexto semestre C#/GatoXa/GatoXa/GatoXa/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GatoXa
+{
+    public partial class MainPage : ContentPage
+    {
+        string turno = "X";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void cambiar(object sender, EventArgs e)
+        {
+            if (turno == "X")
+                turno = "O";
+                
+            else
+            {
+                turno = "X";
+            }
+        }
+        private void ficha(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.Text == "")
+            {
+                btn.Text = turno;
+                cambiar(sender, e);
+            }
+        }
+    }
+}
